2c4a105fa75784dc51aff77a03f4da29dd32c9c6
marinanton <stylesvoboda@ya.ru>
2025-07-18 20:53:30 +0300

    Fix name

--- utils/example.settings.py
+++ utils/example.settings.py
@@ -1,14 +1,12 @@
-
 # Яндекс Диск
-NMAP_APP_PATH = "Приложения/Блокнот картографа Народной карты"
-NMAP_DATA = "index.json"
+nmap_app_path = "Приложения/Блокнот картографа Народной карты"
+nmap_data = "index.json"
 
 # Яндекс
-YDEX_API_KEY = "Ваш OAuth-токен Яндекс"
-YDEX_API_PATH = "https://cloud-api.yandex.net/v1/disk/resources"
+ydex_api_key = "Ваш OAuth-токен Яндекс"
+ydex_api_path = "https://cloud-api.yandex.net/v1/disk/resources"
 
 # Локальные shapefiles
-DEF_SHP_DATA = "Ваш дефолтный путь к папке загрузок браузера/geoserver_data.zip"
-DEF_SHP_NAME = "oopt_wth_detailsPolygon.shp"
-
-
+local_index_path = "index_downloaded.json"
+def_shp_name = "oopt_wth_detailsPolygon.shp"
+def_shp_data = "Ваш дефолтный путь к папке загрузок браузера/geoserver_data.zip"
